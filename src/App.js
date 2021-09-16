985478920f3248bfd804dede08003d16120ccade
naffrey <nick.caffrey@icloud.com>
2021-09-16 13:52:02 -0500

    components added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Here fishyfishy</h1>
+      
     </div>
   );
 }
---
+++ src/components/FishCard/FishCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FishCard(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FishCard;
