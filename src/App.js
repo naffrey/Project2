4968fc47d61a3f19dd6f638e831b4c90305b9579
naffrey <nick.caffrey@icloud.com>
2021-09-16 15:15:57 -0500

    Fish cards have been added to home page as a placeholder till user selects or uses options in header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FishCard from './components/FishCard/FishCard';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <FishCard/>
       
     </div>
   );
--- src/components/FishCard/FishCard.js
+++ src/components/FishCard/FishCard.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import FishCardStyle from "./FishcardStyle.css"
 
 function FishCard(props) {
     return (
-        <div>
-            
-        </div>
-    );
+			<main className='homepageHolder'>
+				<div className='card1'>
+					<img src='' alt='' />
+					<h2 className='fishName'>Bass</h2>
+				</div>
+
+				<div className='card2'>
+					<img src='' alt='' />
+					<h2 className='fishName'>Catfish</h2>
+				</div>
+
+				<div className='card3'>
+					<img src='' alt='' />
+					<h2 className='fishName'>Trout</h2>
+				</div>
+
+				<div className='card4'>
+					<img src='' alt='' />
+					<h2 className='fishName'>Salmon</h2>
+				</div>
+
+				<div className='card5'>
+					<img src='' alt='' />
+					<h2 className='fishName'>Wiper</h2>
+				</div>
+
+				<div className='card6'>
+					<img src='' alt='' />
+					<h2 className="fishName">Sunfish</h2>
+				</div>
+			</main>
+		);
 }
 
 export default FishCard;
