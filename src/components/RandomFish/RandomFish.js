0472a48cbdd87b52038a4a8c76b60fedf6a83906
naffrey <nick.caffrey@icloud.com>
2021-09-17 17:10:20 -0500

    cleaned up RandomFish with map mathod. Still having small issue with API data apearing at state level. researching this weekend to see what steps I need to take in order to have array apear at state level correctly

--- src/components/RandomFish/RandomFish.js
+++ src/components/RandomFish/RandomFish.js
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 import './RandomFishStyle.css';
 
 const RandomFish = (props) => {
-    const [fish, setFish] = useState(null);
+    const [fish, setFish] = useState();
     //const {path} = useParams();
-	const url = `https://www.fishwatch.gov/api/species`;
+	const url = 'https://www.fishwatch.gov/api/species';
+
 
 		useEffect(() => {
 			fetch(url)
 				.then((res) => res.json())
-				.then((json) => {setFish(json)
+				.then((res) => {setFish(res)
 					})
 				.catch(console.error);
 			},[]);
@@ -21,36 +22,44 @@ const RandomFish = (props) => {
 	
 
     return (
-			<div className='randomCard'>
-				<img className='mainFish' src='' alt='' />
-				<p>
-					Maybe I could add a little image selector here for the fish that
-					multiple photos. under the photo. SAME TO THE FISH CARDS.
-				</p>
-				<div className='basicInfo'>
-					<h3>
-						<u>Name</u>
-					</h3>
-					<h4>{fish["species name"]}</h4>
-					<h3>
-						<u>Location</u>
-					</h3>
-					<h4>{fish.Habitat}</h4>
+			<section>
+				{fish.map((fish) => (
+					<div className='randomCard'>
+						<img className='mainFish' src='' alt='' />
+						<p>
+							Maybe I could add a little image selector here for the fish that
+							multiple photos. under the photo. SAME TO THE FISH CARDS.
+						</p>
+						<div className='basicInfo'>
+							<h3>
+								<u>Name</u>
+							</h3>
+							<h4>{fish['species name']}</h4>
+							<h3>
+								<u>Location</u>
+							</h3>
+							<h4>{fish.Habitat}</h4>
 
-					<h3>
-						<u>Physical Description</u>
-					</h3>
-					<h4>{fish.Physical}</h4>
-				</div>
+							<h3>
+								<u>Physical Description</u>
+							</h3>
+							<h4>{fish.Physical}</h4>
+						</div>
 
-				<div className='bio'>
-					<h3>
-						<u>Biology</u>
-					</h3>
-					<p>{fish.biology}</p>
-				</div>
-			</div>
+						<div className='bio'>
+							<h3>
+								<u>Biology</u>
+							</h3>
+							<p>{fish.biology}</p>
+						</div>
+					</div>
+				))}
+			</section>
 		);
+
 }
+
+
+
 
 export default RandomFish;
