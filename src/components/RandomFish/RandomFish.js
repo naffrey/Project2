cb098f3dc5d1a790e7fad0bfc22d1b3da1a4585b
naffrey <nick.caffrey@icloud.com>
2021-09-20 17:40:55 -0500

    Rendered tags inside of strings from API using dangerouslySetInnerHTML

--- src/components/RandomFish/RandomFish.js
+++ src/components/RandomFish/RandomFish.js
@@ -2,34 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './RandomFishStyle.css';
 
 function RandomFish(props) {
-    const [fish, setFish] = useState([]);
-    //const {Path} = useParams();
+	const [fish, setFish] = useState([]);
+	//const {Path} = useParams();
 	const url =
 		'https://cors-anywhere.herokuapp.com/https://www.fishwatch.gov/api/species';
 
-
-		useEffect(() => {
-			function getData() {fetch(url) 
+	useEffect(() => {
+		function getData() {
+			fetch(url)
 				.then((res) => res.json())
 				.then((json) => {
-					console.log(json) 
-					setFish(json)
-					})
-				.catch(console.error);}
-				getData()
-			},[]); 
-
-		if (!fish.length) {
-			return <p>Reeling...</p>;
+					console.log(json);
+					setFish(json);
+				})
+				.catch(console.error);
 		}
-	
+		getData();
+	}, []);
 
-    return (
-			<section className='singleFish'>
-				{fish.map(fishs =>{
+	if (!fish.length) {
+		return <p>Reeling...</p>;
+	}
 
-				return	<div className='randomCard'>
-						<img className='mainFish' src={fishs['Species Illustration Photo'].src} alt={fishs['Species Illustration Photo'].alt} />
+	return (
+		<section className='singleFish'>
+			{fish.map((fishs) => {
+				return (
+					<div className='randomCard'>
+						<img
+							className='mainFish'
+							src={fishs['Species Illustration Photo'].src}
+							alt={fishs['Species Illustration Photo'].alt}
+						/>
 						<p>
 							Maybe I could add a little image selector here for the fish that
 							multiple photos. under the photo. SAME TO THE FISH CARDS.
@@ -42,7 +46,7 @@ function RandomFish(props) {
 							<h3>
 								<u>Location</u>
 							</h3>
-							<h4>{fishs.Location}</h4>
+							<div dangerouslySetInnerHTML={{ __html: fishs.Location }} />
 
 							<h3>
 								<u>Physical Description</u>
@@ -54,16 +58,17 @@ function RandomFish(props) {
 							<h3>
 								<u>Biology</u>
 							</h3>
-							<p>{fishs.Biology}</p>
+
+							<p>
+								<div dangerouslySetInnerHTML={{ __html: fishs.Biology }} />
+								{/* {fishs.Biology} */}
+							</p>
 						</div>
 					</div>
-				})}
-			</section>
-		);
-					
+				);
+			})}
+		</section>
+	);
 }
-
-
-
 
 export default RandomFish;
